51ea6d15042f9990148e89752f923c3abe5b3749
Paul <iampaulmatheson@gmail.com>
2023-02-26 21:32:26 -0800

    Added functions to modify displayed forecast

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/98199?unitGroup=metric&key=JXW9255N8RZ8H7BTWSULKLMXF&contentType=json")
+
+let now = new Date()
+let hourlyTimes = document.querySelectorAll("h4.hour")
+let weeklyDays = document.querySelectorAll("h4.daySignifier")
+
+function returnHour(date) {
+    return date.getHours() > 12 ? date.getHours() - 12 : date.getHours()
+}
+
+function returnDay(date) {
+    switch (date) {
+        case 0:
+            day = "Sun";
+            break;
+        case 1:
+            day = "Mon";
+            break;
+        case 2:
+            day = "Tues";
+            break;
+        case 3:
+            day = "Wed";
+            break;
+        case 4:
+            day = "Thu";
+            break;
+        case 5:
+            day = "Fri";
+            break;
+        case 6:
+            day = "Sat";
+    }
+    return day
+}
+
+function denoteMeridien(x) {
+    if (x.getHours() > 12) {
+        return "PM"
+    } else {
+        return "AM"
+    }
+}
+
+hourlyTimes.forEach((hour, index) => {
+    let currHour = returnHour(now) + index
+    if (index == 0) {
+        hour.innerHTML = "Now"
+    } else {
+        hour.innerHTML = (currHour > 12 ? currHour - 12 : currHour) + " " + denoteMeridien(now)
+    }
+})
+
+weeklyDays.forEach((day, index) => {
+    day.innerHTML = returnDay(now.getDay() + index)
+})
