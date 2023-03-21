faba34d0307fd2e3fc3ff56a1eb05cf4a7208ed6
paulmatheson <iampaulmatheson@gmail.com>
2023-03-20 17:54:28 -0700

    Modified

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-require('dotenv').config()
+require('dotenv').config();
 
-api_key = process.env.API_KEY
+const fetch = require('node-fetch');
 
-const express = require('express')
-const app = express()
-app.listen(3000, () => console.log('Listening at 3000'))
-app.use(express.static('public'))
-app.use(express.json({ limit: '1mb' }))
+const api_key = process.env.API_KEY;
+
+const express = require('express');
+const app = express();
+app.listen(3000, () => console.log('Listening at 3000'));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
 
 app.get('/weather/:locinfo', async (request, response) => {
+    const params = request.params.locinfo.split(',');
+    const loc = params[0];
+    const unit = params[1];
 
-    const params = request.params.locinfo.split(',')
-    const loc = params[0]
-    const unit = params[1]
+    const api_url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}?unitGroup=${unit}&key=${api_key}&contentType=json`;
 
-    const api_url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}?unitGroup=${unit}&key=${api_key}&contentType=json`
-
-    const fetch_response = await fetch(api_url)
-    const json = await fetch_response.json()
-    response.json(json)
-})
+    const fetch_response = await fetch(api_url);
+    const json = await fetch_response.json();
+    response.json(json);
+});
