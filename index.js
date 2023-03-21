4e2e606b5976dccf24b61f35312e4378681a68ce
paulmatheson <iampaulmatheson@gmail.com>
2023-03-20 20:25:19 -0700

    Revert "Added build command"
    This reverts commit 2452b2886248d88384a1d4f203c41f9408e87a7c.

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 require('dotenv').config()
-const fetch = require("node-fetch-commonjs")
-const express = require('express')
 
 api_key = process.env.API_KEY
 
+const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Starting server at ${port}`);
-});
-
+app.listen(3000, () => console.log('Listening at 3000'))
 app.use(express.static('public'))
 app.use(express.json({ limit: '1mb' }))
 
