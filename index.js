24b629063854e8f004f63bed6ad129cdec5380b9
paulmatheson <iampaulmatheson@gmail.com>
2023-03-20 18:14:46 -0700

    Modifying the call

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
-
+const express = require('express');
 api_key = process.env.API_KEY
 
-const express = require('express')
-const app = express()
-app.listen(3000, () => console.log('Listening at 3000'))
-app.use(express.static('public'))
-app.use(express.json({ limit: '1mb' }))
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Starting server at ${port}`);
+});
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
 
 app.get('/weather/:locinfo', async (request, response) => {
 
