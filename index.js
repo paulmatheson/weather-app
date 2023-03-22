027df0842a855d67ebe29eb5061bca6f2646fa68
paulmatheson <iampaulmatheson@gmail.com>
2023-03-21 21:10:40 -0700

    Final touches to the MVP

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const api_key = process.env.API_KEY;
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
     console.log(`Starting server at ${port}`);
 });
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
-
-console.log(api_key);
 
 app.get('/weather/:locinfo', async (request, response) => {
     console.log(request.url);
