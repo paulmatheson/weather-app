33efe2f3bc9708119338ce0ce7f1c58e76e42bd9
paulmatheson <iampaulmatheson@gmail.com>
2024-02-16 19:32:53 -0800

    Fixing bug with API call

--- functions/weather.js
+++ functions/weather.js
@@ -26,32 +26,4 @@ exports.handler = async function (event, context) {
             body: JSON.stringify({ error: 'Internal Server Error' }),
         };
     }
-};
-
-require('dotenv').config()
-const fetch = require('node-fetch');
-const api_key = process.env.API_KEY;
-
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Starting server at ${port}`);
-});
-app.use(express.static('public'));
-app.use(express.json({ limit: '1mb' }));
-
-app.get('/weather/:locinfo', async (request, response) => {
-    console.log(request.url);
-
-    const params = request.params.locinfo.split(',')
-    const loc = params[0]
-    const unit = params[1]
-
-    const api_url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}?unitGroup=${unit}&key=${api_key}&contentType=json`
-
-    const fetch_response = await fetch(api_url)
-    const json = await fetch_response.json()
-    response.json(json)
-})
+}
